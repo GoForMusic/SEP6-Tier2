6068d76fef79611b2db241d4fe36378ad835ef89
radum <radumarian250797@gmail.com>
2023-11-06 15:26:13 +0100

    Forgot about the interface

--- RestServer/Data/IDataAccess.cs
+++ RestServer/Data/IDataAccess.cs
@@ -1,9 +1,12 @@
-﻿namespace RestServer.Data
+﻿using Shared;
+
+namespace RestServer.Data
 {
     /// <summary>
     /// Class where methods to interact with db can be found
     /// </summary>
     public interface IDataAccess
     {
+        public Task<Account> CreateAccount(Account account);
     }
 }
