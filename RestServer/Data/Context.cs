60fac9c4749dd7df480aad526e2c2a4424a81246
radum <radumarian250797@gmail.com>
2023-11-24 16:25:44 +0100

    Created DAO Test

--- RestServer/Data/Context.cs
+++ RestServer/Data/Context.cs
@@ -9,7 +9,6 @@ namespace RestServer.Data
     /// </summary>
     public class Context : DbContext
     {
-
         public DbSet<Account> Accounts { get; set; }
 
         /// <summary>
@@ -18,21 +17,29 @@ namespace RestServer.Data
         /// <param name="optionsBuilder"></param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source = movies.db");
+            if (!optionsBuilder.IsConfigured)
+            {
+                // UseSqlite or other database provider configuration
+                optionsBuilder.UseSqlite("Data Source = movies.db");
+            }
+        }
+        /// <summary>
+        /// Constructor for testing
+        /// </summary>
+        /// <param name="options"></param>
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
         }
 
         /// <summary>
-        /// Set up keys and automatic id 
+        /// Set up keys and automatic id, required fields, etc 
         /// </summary>
         /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Account>().Property(t => t.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<Account>().HasKey(t => t.Id);
-            //modelBuilder.Entity<Player>().Property(s => s.ID).ValueGeneratedOnAdd();
-            //modelBuilder.Entity<Player>().HasKey(t => t.ID);
-            //modelBuilder.Entity<HoleScore>().Property(s => s.ID).ValueGeneratedOnAdd();
-            //modelBuilder.Entity<HoleScore>().HasKey(t => t.ID);
+            modelBuilder.Entity<Account>().Property(t => t.Password).IsRequired(false);
         }
     }
 }
