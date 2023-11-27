9973f3e7f586950c1140c9a08780b051927a46a4
Adrian <militaruadriancristian@gmail.com>
2023-11-27 13:21:29 +0100

    fix swagger

--- RestServer/Program.cs
+++ RestServer/Program.cs
@@ -17,11 +17,10 @@ builder.Services.AddScoped<DataAccess>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
