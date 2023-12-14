0bc9a13129a850c369278f66c1ea460f8adce0f2
Adrian <militaruadriancristian@gmail.com>
2023-12-14 19:21:01 +0100

    Add the AVG functions for actors base on their movies

--- RestServer/Data/DAOImplementation/StarsDao.cs
+++ RestServer/Data/DAOImplementation/StarsDao.cs
@@ -26,4 +26,23 @@ public class StarsDao : IStarsDao
             .Where(s => s.movie_id.Id == movieid)
             .ToListAsync();
     }
+
+    /// <inheritdoc />
+    public async Task<List<Ratings>> GetDirectorsByName(long actorId)
+    {
+       List<Stars> stars = await _context.Stars!
+            .Include(s => s.movie_id)
+            .Include(s => s.person_id)
+            .Where(s => s.person_id.Id == actorId)
+            .ToListAsync();
+
+       List<Ratings> ratingsList = new List<Ratings>();
+
+       foreach (var variablStar in stars)
+       {
+           ratingsList.Add(await _context.Ratings!.Include(r=>r.movie_id).FirstAsync(r=>r.movie_id.Id==variablStar.movie_id.Id));
+       }
+       
+       return ratingsList;
+    }
 }
